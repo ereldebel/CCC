68edc85a26529dfd1c2befe6173cf1ac9c646555
Erel Debel <0ereldebel0@gmail.com>
2023-05-22 13:10:41 +0300

    Add SceneLoader.Reset scene that unloads all but SceneLoader scene and reloads loadOnStart scenes. Change SceneLoader.SwitchScene API to get a collection of scenes to unload instead of a single scene. Add SceneLoader.SwitchScene method that gets a collection of scenes to load. Add SceneLoader.UnloadScenes method. Changed SwitchScene coroutine to use unscaled time

--- Runtime/SceneLoader.cs
+++ Runtime/SceneLoader.cs
@@ -13,15 +13,17 @@ namespace CCC.Runtime
 		#region Serialized Fields
 
 		[field: SerializeField] public SceneEntry[] Scenes { get; private set; }
-		
+
 		[SerializeField] private float sceneSwitchFadeDuration = 2;
 		[SerializeField] private CanvasGroup transitionScreen;
 
 		#endregion
 
-		#region Non-Serialized Fields
+		#region Private Fields
 
 		private readonly ActiveScenes _activeScenes = new();
+
+		private readonly SceneEntry[] _singleScene = new SceneEntry[1];
 
 		#endregion
 
@@ -32,19 +34,25 @@ namespace CCC.Runtime
 			foreach (var scene in Scenes)
 			{
 				if (IsSceneLoaded(scene))
-				{
 					_activeScenes.Add(scene);
-				}
 				else if (scene.loadOnStart)
-				{
 					LoadScene(scene);
-				}
 			}
 		}
 
 		#endregion
 
 		#region Public Methods
+
+		/// <summary>
+		/// Unloads all but the SceneLoader scene and reloads all scenes marked to load on start.
+		/// </summary>
+		public void Reset()
+		{
+			var scenesToUnload = _activeScenes[SceneMask.InverseMask(SceneType.ConstantReload)].ToList();
+			var scenesToLoad = Scenes.Where(scene => scene.loadOnStart && !IsSceneLoaded(scene)).ToList();
+			SwitchScene(scenesToLoad, scenesToUnload);
+		}
 
 		/// <summary>
 		/// Gets all active scenes of requested type.
@@ -58,22 +66,36 @@ namespace CCC.Runtime
 		/// Unloads all dynamic scenes, reloads ConstantReload scenes and loads the given scene.
 		/// </summary>
 		/// <param name="sceneEntryIndex">The index of the SceneEntry in the SceneLoader.</param>
-		/// <param name="specificSceneToUnload">Optional: a non-dynamic scene to unload.</param>
+		/// <param name="specificScenesToUnload">Optional: specific scenes to unload. This is instead of the dynamic scenes unload</param>
 		/// <param name="switchEndAction">Optional: an action to perform once the Switch ends.</param>
-		public void SwitchScene(int sceneEntryIndex, SceneEntry specificSceneToUnload = null,
+		public void SwitchScene(int sceneEntryIndex, IReadOnlyCollection<SceneEntry> specificScenesToUnload = null,
 			Action switchEndAction = null) =>
-			SwitchScene(Scenes[sceneEntryIndex], specificSceneToUnload, switchEndAction);
+			SwitchScene(Scenes[sceneEntryIndex], specificScenesToUnload, switchEndAction);
 
 		/// <summary>
 		/// Unloads all dynamic scenes, reloads ConstantReload scenes and loads the given scene.
 		/// </summary>
 		/// <param name="scene">Scene to load</param>
-		/// <param name="specificSceneToUnload">Optional: a non-dynamic scene to unload.</param>
+		/// <param name="specificScenesToUnload">Optional: specific scenes to unload. This is instead of the dynamic scenes unload</param>
 		/// <param name="switchEndAction">Optional: an action to perform once the Switch ends.</param>
-		public void SwitchScene(SceneEntry scene, SceneEntry specificSceneToUnload = null,
+		public void SwitchScene(SceneEntry scene, IReadOnlyCollection<SceneEntry> specificScenesToUnload = null,
 			Action switchEndAction = null)
 		{
-			StartCoroutine(SwitchSceneCoroutine(scene, specificSceneToUnload, switchEndAction));
+			_singleScene[0] = scene;
+			SwitchScene(_singleScene, specificScenesToUnload, switchEndAction);
+		}
+
+		/// <summary>
+		/// Unloads all dynamic scenes, reloads ConstantReload scenes and loads the given scene.
+		/// </summary>
+		/// <param name="scenes">Scenes to load</param>
+		/// <param name="specificScenesToUnload">Optional: specific scenes to unload. This is instead of the dynamic scenes unload</param>
+		/// <param name="switchEndAction">Optional: an action to perform once the Switch ends.</param>
+		public void SwitchScene(IReadOnlyCollection<SceneEntry> scenes,
+			IReadOnlyCollection<SceneEntry> specificScenesToUnload = null,
+			Action switchEndAction = null)
+		{
+			StartCoroutine(SwitchSceneCoroutine(scenes, specificScenesToUnload, switchEndAction));
 		}
 
 		/// <summary>
@@ -103,15 +125,25 @@ namespace CCC.Runtime
 		}
 
 		/// <summary>
-		/// Unloads active all dynamic scenes.
+		/// Unloads the given scenes.
+		/// </summary>
+		public void UnloadScenes(IEnumerable<SceneEntry> scenes)
+		{
+			foreach (var scene in scenes)
+			{
+				SceneManager.UnloadSceneAsync(scene.sceneName);
+				_activeScenes.Remove(scene);
+			}
+		}
+
+		/// <summary>
+		/// Unloads all active dynamic scenes.
 		/// </summary>
 		public void UnloadDynamicScenes()
 		{
 			var sceneEntries = _activeScenes[SceneType.Dynamic];
 			foreach (var scene in sceneEntries)
-			{
 				SceneManager.UnloadSceneAsync(scene.sceneName);
-			}
 
 			sceneEntries.Clear();
 		}
@@ -132,29 +164,29 @@ namespace CCC.Runtime
 			return false;
 		}
 
-		private IEnumerator SwitchSceneCoroutine(SceneEntry newScene, SceneEntry specificSceneToUnload = null,
+		private IEnumerator SwitchSceneCoroutine(IReadOnlyCollection<SceneEntry> newScenes,
+			IReadOnlyCollection<SceneEntry> specificScenesToUnload = null,
 			Action switchEndAction = null)
 		{
-			yield return Coroutines.Interpolate(t => transitionScreen.alpha = t, sceneSwitchFadeDuration);
-			if (specificSceneToUnload == null)
-				UnloadDynamicScenes();
+			yield return Coroutines.InterpolateUnscaledTime(t => transitionScreen.alpha = t, sceneSwitchFadeDuration);
+			if (specificScenesToUnload != null)
+				yield return UnloadScenes(specificScenesToUnload);
 			else
-				UnloadScene(specificSceneToUnload);
+				yield return UnloadScenes(SceneType.Dynamic);
 			if (_activeScenes[SceneType.ConstantReload].Count > 0)
 				yield return ReloadScenes();
-			yield return PerformSingleAsyncOperation(SceneManager.LoadSceneAsync(newScene.sceneName,
-				LoadSceneMode.Additive));
-			yield return Coroutines.Interpolate(t => transitionScreen.alpha = 1 - t, sceneSwitchFadeDuration);
-			_activeScenes.Add(newScene);
+			yield return LoadScenes(newScenes);
+			yield return Coroutines.InterpolateUnscaledTime(t => transitionScreen.alpha = 1 - t, sceneSwitchFadeDuration);
 			switchEndAction?.Invoke();
 		}
 
 		private IEnumerator ReloadScenes()
 		{
-			int constantReloadCount = _activeScenes[SceneType.ConstantReload].Count;
+			var constantReloadScenes = _activeScenes[SceneType.ConstantReload];
+			int constantReloadCount = constantReloadScenes.Count;
 			var ops = new AsyncOperation[constantReloadCount];
 			int i = 0;
-			foreach (var reloadScene in _activeScenes[SceneType.ConstantReload])
+			foreach (var reloadScene in constantReloadScenes)
 			{
 				ops[i++] = SceneManager.UnloadSceneAsync(reloadScene.sceneName);
 			}
@@ -162,7 +194,7 @@ namespace CCC.Runtime
 			yield return new WaitUntil(() => ops.All(op => op.isDone));
 
 			i = 0;
-			foreach (var reloadScene in _activeScenes[SceneType.ConstantReload])
+			foreach (var reloadScene in constantReloadScenes)
 			{
 				ops[i++] = SceneManager.LoadSceneAsync(reloadScene.sceneName, LoadSceneMode.Additive);
 			}
@@ -170,10 +202,44 @@ namespace CCC.Runtime
 			yield return new WaitUntil(() => ops.All(op => op.isDone));
 		}
 
-		private static IEnumerator PerformSingleAsyncOperation(AsyncOperation op)
+		private IEnumerator UnloadScenes(IReadOnlyCollection<SceneEntry> scenes)
 		{
-			while (!op.isDone)
-				yield return null;
+			int sceneCount = scenes.Count;
+			var ops = new AsyncOperation[sceneCount];
+			int i = 0;
+			foreach (var scene in scenes)
+			{
+				ops[i++] = SceneManager.UnloadSceneAsync(scene.sceneName);
+				_activeScenes.Remove(scene);
+			}
+
+			yield return new WaitUntil(() => ops.All(op => op.isDone));
+		}
+
+		private IEnumerator UnloadScenes(SceneType type)
+		{
+			var scenes = _activeScenes[type];
+			int sceneCount = scenes.Count;
+			var ops = new AsyncOperation[sceneCount];
+			int i = 0;
+			foreach (var scene in scenes)
+				ops[i++] = SceneManager.UnloadSceneAsync(scene.sceneName);
+			_activeScenes[type].Clear();
+			yield return new WaitUntil(() => ops.All(op => op.isDone));
+		}
+
+		private IEnumerator LoadScenes(IReadOnlyCollection<SceneEntry> scenes)
+		{
+			int sceneCount = scenes.Count;
+			var ops = new AsyncOperation[sceneCount];
+			int i = 0;
+			foreach (var scene in scenes)
+			{
+				ops[i++] = SceneManager.LoadSceneAsync(scene.sceneName, LoadSceneMode.Additive);
+				_activeScenes.Add(scene);
+			}
+
+			yield return new WaitUntil(() => ops.All(op => op.isDone));
 		}
 
 		#endregion
@@ -182,15 +248,27 @@ namespace CCC.Runtime
 
 		private class ActiveScenes
 		{
+			#region Private Fields
+
 			private readonly HashSet<SceneEntry>[] _scenes;
 
-			/// <summary>
-			/// Get a HashSet of all active scenes by type;
-			/// </summary>
-			public HashSet<SceneEntry> this[SceneType type] => _scenes[(ushort)type];
+			#endregion
 
-			internal void Add(SceneEntry scene) => this[scene.type].Add(scene);
-			internal void Remove(SceneEntry scene) => this[scene.type].Remove(scene);
+			#region Properties
+
+			/// <summary>
+			/// Get a HashSet of all active scenes by type.
+			/// </summary>
+			internal HashSet<SceneEntry> this[SceneType type] => _scenes[(int)type];
+
+			/// <summary>
+			/// Get an enumerable of all actives scenes of the given types. 
+			/// </summary>
+			internal IEnumerable<SceneEntry> this[SceneMask types] => new EnumerableScenes(types, this);
+
+			#endregion
+
+			#region Conostructors
 
 			internal ActiveScenes()
 			{
@@ -199,6 +277,47 @@ namespace CCC.Runtime
 				for (int i = 0; i < typeCount; ++i)
 					_scenes[i] = new();
 			}
+
+			#endregion
+
+			#region Internal Methods
+
+			internal void Add(SceneEntry scene) => this[scene.type].Add(scene);
+
+			internal void Append(IEnumerable<SceneEntry> scenes)
+			{
+				foreach (var scene in scenes)
+					this[scene.type].Add(scene);
+			}
+
+			internal void Remove(SceneEntry scene) => this[scene.type].Remove(scene);
+
+			#endregion
+
+			#region Types
+
+			private class EnumerableScenes : IEnumerable<SceneEntry>
+			{
+				private readonly SceneMask _types;
+				private readonly ActiveScenes _activeScenes;
+
+				internal EnumerableScenes(SceneMask types, ActiveScenes activeScenes)
+				{
+					_types = types;
+					_activeScenes = activeScenes;
+				}
+
+				public IEnumerator<SceneEntry> GetEnumerator()
+				{
+					foreach (var type in _types)
+						foreach (var entry in _activeScenes[type])
+							yield return entry;
+				}
+
+				IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+			}
+
+			#endregion
 		}
 
 		[Serializable]
@@ -220,7 +339,7 @@ namespace CCC.Runtime
 			[SerializeField] public bool loadOnStart;
 		}
 
-		public enum SceneType : ushort
+		public enum SceneType
 		{
 			/// <summary>
 			/// The SceneLoader's scene.
@@ -241,6 +360,53 @@ namespace CCC.Runtime
 			/// A scene that is reloaded on each scene switch.
 			/// </summary>
 			ConstantReload = 3,
+		}
+
+		public readonly struct SceneMask
+		{
+			#region Private Fields
+
+			private readonly int _mask;
+
+			#endregion
+
+			#region Constructors
+
+			public SceneMask(int mask) => _mask = mask;
+
+			public SceneMask(params SceneType[] types) =>
+				_mask = types.Aggregate(0, (mask, type) => mask | (int)type);
+
+			public static SceneMask InverseMask(SceneType type) =>
+				new(~type);
+
+			public static SceneMask InverseMask(params SceneType[] types) =>
+				new(~types.Aggregate(0, (mask, type) => mask | (int)type));
+
+			#endregion
+
+			#region Operator Overloads
+
+			public static explicit operator SceneMask(int mask) => new(mask);
+
+			public static explicit operator SceneMask(SceneType type) => new(1 << (int)type);
+
+			public static SceneMask operator &(SceneMask lhs, SceneMask rhs) => new(lhs._mask & rhs._mask);
+
+			#endregion
+
+			#region Public Methods
+
+			public IEnumerator<SceneType> GetEnumerator()
+			{
+				const int bit = 1;
+				int typeMask = _mask;
+				for (int type = 0; type < EnumUtils.Count<SceneType>(); ++type, typeMask >>= 1)
+					if ((typeMask & bit) != 0)
+						yield return (SceneType)type;
+			}
+
+			#endregion
 		}
 
 		#endregion
