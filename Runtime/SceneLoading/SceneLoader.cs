b1920751efc03a7b72db0e76e38e43cd34b2c43e
ereldebel <ereldebel@gmail.com>
2026-02-02 18:44:47 +0200

    package naming and coding standards

--- Runtime/SceneLoading/SceneLoader.cs
+++ Runtime/SceneLoading/SceneLoader.cs
@@ -18,18 +18,17 @@ namespace CCC.Runtime.SceneLoading
 	{
 		#region Serialized Fields
 
-		[field: SerializeField,
-			Tooltip("Array of scene entries defining all available scenes and their properties")]
+		[field: SerializeField, Tooltip("Array of scene entries defining all available scenes and their properties")]
 		public SceneEntry[] Scenes { get; private set; }
 
 		[field: SerializeField, Tooltip("UI group used for transition screen effects")]
 		public UIGroup TransitionScreen { get; set; }
 
 		[SerializeField, Tooltip("Duration of fade transitions in seconds")]
-		private float sceneSwitchFadeDuration = 2;
+		private float _sceneSwitchFadeDuration = 2;
 
 		[SerializeField, Tooltip("Minimum time to show loading screen in seconds")]
-		private float minLoadTime = 1.5f;
+		private float _minLoadTime = 1.5f;
 
 		#endregion
 
@@ -69,20 +68,32 @@ namespace CCC.Runtime.SceneLoading
 			ExitTransition ??= t => TransitionScreen.Opacity = 1 - t;
 
 			var blankSlate = true;
-			foreach (var scene in Scenes)
+			foreach (SceneEntry scene in Scenes)
 			{
-				if (!IsSceneLoaded(scene)) continue;
+				if (!IsSceneLoaded(scene))
+				{
+					continue;
+				}
+
 				_activeScenes.Add(scene);
+
 				if (scene.type != SceneType.SceneLoader)
+				{
 					blankSlate = false;
+				}
 			}
 
 			if (!blankSlate)
+			{
 				return;
-			foreach (var scene in Scenes)
+			}
+
+			foreach (SceneEntry scene in Scenes)
 			{
 				if (scene.loadOnStart && scene.type != SceneType.SceneLoader)
+				{
 					LoadScene(scene);
+				}
 			}
 		}
 
@@ -94,10 +105,10 @@ namespace CCC.Runtime.SceneLoading
 		/// Unloads all but the SceneLoader scene and reloads all scenes marked to load on start.
 		/// </summary>
 		/// <returns>A UniTask representing the asynchronous reset operation.</returns>
-		public UniTask Reset()
+		public UniTask ResetScenes()
 		{
-			var scenesToUnload = _activeScenes[SceneMask.InverseMask(SceneType.SceneLoader)].ToList();
-			var scenesToLoad = Scenes.Where(scene => scene.loadOnStart && !IsSceneLoaded(scene)).ToList();
+			List<SceneEntry> scenesToUnload = _activeScenes[SceneMask.InverseMask(SceneType.SceneLoader)].ToList();
+			List<SceneEntry> scenesToLoad = Scenes.Where(scene => scene.loadOnStart && !IsSceneLoaded(scene)).ToList();
 			return SwitchScene(scenesToLoad, scenesToUnload);
 		}
 
@@ -141,19 +152,25 @@ namespace CCC.Runtime.SceneLoading
 		public async UniTask SwitchScene(IReadOnlyCollection<SceneEntry> newScenes,
 			IReadOnlyCollection<SceneEntry> specificScenesToUnload = null)
 		{
-			await UniTaskUtils.Interpolate(EntryTransition, sceneSwitchFadeDuration, ignoreTimeScale: true);
-			UniTask minLoadTimer = UniTaskUtils.Delay(minLoadTime);
+			await UniTaskUtils.Interpolate(EntryTransition, _sceneSwitchFadeDuration, ignoreTimeScale: true);
+			UniTask minLoadTimer = UniTaskUtils.Delay(_minLoadTime);
 
 			if (specificScenesToUnload != null)
+			{
 				await UnloadScenesAsync(specificScenesToUnload);
+			}
 			else
+			{
 				await UnloadScenesAsync(SceneType.Dynamic);
+			}
 
 			if (_activeScenes[SceneType.ConstantReload].Any())
+			{
 				await ReloadScenesAsync();
+			}
 
 			await UniTask.WhenAll(LoadScenesAsync(newScenes), minLoadTimer);
-			await UniTaskUtils.Interpolate(ExitTransition, sceneSwitchFadeDuration, ignoreTimeScale: true);
+			await UniTaskUtils.Interpolate(ExitTransition, _sceneSwitchFadeDuration, ignoreTimeScale: true);
 		}
 
 		/// <summary>
@@ -194,7 +211,7 @@ namespace CCC.Runtime.SceneLoading
 		/// <param name="scenes">The collection of scene entries to unload.</param>
 		public void UnloadScenes(IEnumerable<SceneEntry> scenes)
 		{
-			foreach (var scene in scenes)
+			foreach (SceneEntry scene in scenes)
 			{
 				SceneManager.UnloadSceneAsync(scene.sceneName);
 				_activeScenes.Remove(scene);
@@ -220,11 +237,13 @@ namespace CCC.Runtime.SceneLoading
 		/// <returns>True if the scene is loaded, false otherwise.</returns>
 		private bool IsSceneLoaded(SceneEntry scene)
 		{
-			var numOfLoadedScenes = SceneManager.sceneCount;
+			int numOfLoadedScenes = SceneManager.sceneCount;
 			for (int i = 0; i < numOfLoadedScenes; ++i)
 			{
 				if (SceneManager.GetSceneAt(i).name == scene.sceneName)
+				{
 					return true;
+				}
 			}
 
 			return false;
@@ -236,12 +255,12 @@ namespace CCC.Runtime.SceneLoading
 		/// <returns>A UniTask representing the asynchronous reload operation.</returns>
 		private async UniTask ReloadScenesAsync()
 		{
-			var constantReloadScenes = _activeScenes[SceneType.ConstantReload];
+			HashSet<SceneEntry> constantReloadScenes = _activeScenes[SceneType.ConstantReload];
 			int constantReloadCount = constantReloadScenes.Count;
 			var tasks = new UniTask[constantReloadCount];
 
 			int i = 0;
-			foreach (var reloadScene in constantReloadScenes)
+			foreach (SceneEntry reloadScene in constantReloadScenes)
 			{
 				tasks[i++] = SceneManager.UnloadSceneAsync(reloadScene.sceneName).ToUniTask();
 			}
@@ -249,7 +268,7 @@ namespace CCC.Runtime.SceneLoading
 			await UniTask.WhenAll(tasks);
 
 			i = 0;
-			foreach (var reloadScene in constantReloadScenes)
+			foreach (SceneEntry reloadScene in constantReloadScenes)
 			{
 				tasks[i++] = SceneManager.LoadSceneAsync(reloadScene.sceneName, LoadSceneMode.Additive).ToUniTask();
 			}
@@ -268,7 +287,7 @@ namespace CCC.Runtime.SceneLoading
 			var tasks = new UniTask[sceneCount];
 
 			int i = 0;
-			foreach (var scene in scenes)
+			foreach (SceneEntry scene in scenes)
 			{
 				tasks[i++] = SceneManager.UnloadSceneAsync(scene.sceneName)
 					.ToUniTask()
@@ -296,7 +315,7 @@ namespace CCC.Runtime.SceneLoading
 			var tasks = new UniTask[sceneCount];
 
 			int i = 0;
-			foreach (var scene in scenes)
+			foreach (SceneEntry scene in scenes)
 			{
 				tasks[i++] = SceneManager.LoadSceneAsync(scene.sceneName, LoadSceneMode.Additive)
 					.ToUniTask()
@@ -317,7 +336,7 @@ namespace CCC.Runtime.SceneLoading
 		{
 			#region Private Fields
 
-			public readonly HashSet<SceneEntry>[] _scenes;
+			private readonly HashSet<SceneEntry>[] _scenes;
 
 			#endregion
 
@@ -346,10 +365,13 @@ namespace CCC.Runtime.SceneLoading
 			/// </summary>
 			internal ActiveScenes()
 			{
-				var typeCount = EnumUtils.Count<SceneType>();
+				int typeCount = EnumUtils.Count<SceneType>();
 				_scenes = new HashSet<SceneEntry>[typeCount];
+
 				for (int i = 0; i < typeCount; ++i)
+				{
 					_scenes[i] = new();
+				}
 			}
 
 			#endregion
@@ -368,8 +390,10 @@ namespace CCC.Runtime.SceneLoading
 			/// <param name="scenes">The collection of scene entries to add.</param>
 			internal void Append(IEnumerable<SceneEntry> scenes)
 			{
-				foreach (var scene in scenes)
+				foreach (SceneEntry scene in scenes)
+				{
 					this[scene.type].Add(scene);
+				}
 			}
 
 			/// <summary>
@@ -407,9 +431,13 @@ namespace CCC.Runtime.SceneLoading
 				/// <returns>An enumerator for SceneEntry objects.</returns>
 				public IEnumerator<SceneEntry> GetEnumerator()
 				{
-					foreach (var type in _types)
-						foreach (var entry in _activeScenes[type])
+					foreach (SceneType type in _types)
+					{
+						foreach (SceneEntry entry in _activeScenes[type])
+						{
 							yield return entry;
+						}
+					}
 				}
 
 				/// <summary>
@@ -418,7 +446,6 @@ namespace CCC.Runtime.SceneLoading
 				/// <returns>A non-generic enumerator.</returns>
 				IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 			}
-
 			#endregion
 		}
 		#endregion
